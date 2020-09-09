b8619545d65a455c2a6e04f3a0b11dbee10f1b17
chrisvoncsefalvay <chris@chrisvoncsefalvay.com>
2020-09-08 20:12:57 -0400

    Bump version: 0.1.9 → 0.2.0

--- sadie/__init__.py
+++ sadie/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Chris von Csefalvay"""
 __email__ = 'chris@chrisvoncsefalvay.com'
-__version__ = '0.1.9'
+__version__ = '0.2.0'
 
 from sadie.models.base import *  # noqa
 from sadie.models.simple import *  # noqa
